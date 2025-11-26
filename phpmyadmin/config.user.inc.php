3b05bbe15e1c0d5451b9876661289d0bd32e7bfd
Karen Hamazaspyan <khamazaspyan@gmail.com>
2025-11-26 23:27:42 +0400

    Update PMA config

---
+++ phpmyadmin/config.user.inc.php
@@ -0,0 +1,11 @@
+<?php
+/* Custom config for phpMyAdmin behind a reverse proxy */
+
+$cfg['PmaAbsoluteUri'] = 'http://db.rollama.com/';
+$cfg['ForceSSL'] = false;
+
+/* Optional: Increase login cookie validity */
+$cfg['LoginCookieValidity'] = 3600;
+
+/* Use secret blowfish key for cookies */
+$cfg['blowfish_secret'] = 'some_random_secret_key_here';
